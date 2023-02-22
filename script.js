2b7fe3ca0d61d0a731d285fbbdbcf9256a692847
Deo-wx <65434087+Deo-wx@users.noreply.github.com>
2023-02-22 15:46:19 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var myVar;
+
+function myFunction() {
+    myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("myDiv").style.display = "block";
+}
+
+document.getElementById("e-message").onclick = function() {
+    alert("Hello, Please provide a descriptive subject line and your preferred contact method so I can get in touch with you. Thank you very much.");
+}
+
